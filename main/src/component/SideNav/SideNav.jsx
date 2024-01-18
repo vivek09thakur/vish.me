2721e495c0f0721bf91ce23eb1d8924bb6013950
Vivek Thakur <20509.vivek@gpranchi.org>
2024-01-18 23:57:30 +0530

    commit

--- main/src/component/SideNav/SideNav.jsx
+++ main/src/component/SideNav/SideNav.jsx
@@ -1,18 +1,38 @@
-import reactlogo from "../../assets/react.svg";
+import {
+  AiOutlineCodepen,
+  AiFillGithub,
+  AiFillLinkedin,
+  AiOutlineTwitter,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+import userData from "../../data.json";
 import "./SideNav.css";
 
 const SideNav = () => {
+  const {github,linkedin,instagram,twitter,codepen} = userData.user.socials;
+  const socialLinks = {github,linkedin,instagram,twitter,codepen}
+  const Icons = {
+    github: AiFillGithub,
+    linkedin: AiFillLinkedin,
+    instagram: AiOutlineInstagram,
+    twitter: AiOutlineTwitter,
+    codepen: AiOutlineCodepen,
+  }
+ 
+
   return (
     <div className="sideNav">
       <div className="container">
         <div className="line"></div>
         <div className="social_box">
-          <img src={reactlogo} className="icons" />
-          <img src={reactlogo} className="icons" />
-          <img src={reactlogo} className="icons" />
-          <img src={reactlogo} className="icons" />
-          <img src={reactlogo} className="icons" />
-          <img src={reactlogo} className="icons" />
+          {Object.entries(socialLinks).map(([key,link],index)=>{
+            const Icon = Icons[key]
+            return(
+              <a href={link} key={index} target="_blank" rel="noreferrer">
+                <Icon className="icon"/>
+              </a>
+            )
+          })}
         </div>
       </div>
     </div>
