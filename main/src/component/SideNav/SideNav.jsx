733a1843447634fa77114306bbbd54cd3d30e486
Vivek Thakur <20509.vivek@gpranchi.org>
2024-01-18 23:57:51 +0530

    commit

--- main/src/component/SideNav/SideNav.jsx
+++ main/src/component/SideNav/SideNav.jsx
@@ -9,29 +9,29 @@ import userData from "../../data.json";
 import "./SideNav.css";
 
 const SideNav = () => {
-  const {github,linkedin,instagram,twitter,codepen} = userData.user.socials;
-  const socialLinks = {github,linkedin,instagram,twitter,codepen}
+  const { github, linkedin, instagram, twitter, codepen } =
+    userData.user.socials;
+  const socialLinks = { github, linkedin, instagram, twitter, codepen };
   const Icons = {
     github: AiFillGithub,
     linkedin: AiFillLinkedin,
     instagram: AiOutlineInstagram,
     twitter: AiOutlineTwitter,
     codepen: AiOutlineCodepen,
-  }
- 
+  };
 
   return (
     <div className="sideNav">
       <div className="container">
         <div className="line"></div>
         <div className="social_box">
-          {Object.entries(socialLinks).map(([key,link],index)=>{
-            const Icon = Icons[key]
-            return(
+          {Object.entries(socialLinks).map(([key, link], index) => {
+            const Icon = Icons[key];
+            return (
               <a href={link} key={index} target="_blank" rel="noreferrer">
-                <Icon className="icon"/>
+                <Icon className="icon" />
               </a>
-            )
+            );
           })}
         </div>
       </div>
