e26c7480754777c0ae2bb4f8c743aafdcdc12932
Vivek Thakur <20509.vivek@gpranchi.org>
2024-01-22 23:34:59 +0530

    commit

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,10 +1,12 @@
 import SideNav from "./component/SideNav/SideNav";
+import Home from "./component/Home/Home";
 import "./App.css";
 
 function App() {
   return (
     <>
       <SideNav />
+      <Home />
     </>
   );
 }
---
+++ main/src/component/Home/Home.jsx
@@ -0,0 +1,11 @@
+import "./Styles/Style.css";
+
+const Home = () =>{
+    return(
+        <div className="home">
+            <h3 className="title">Hello</h3>
+        </div>
+    )
+}
+
+export default Home;
