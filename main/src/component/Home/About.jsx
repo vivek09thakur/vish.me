20b536158eda95de42723e16d523bae3c67700a2
Vivek Thakur <20509.vivek@gpranchi.org>
2024-01-24 18:20:26 +0530

    commit

--- main/src/component/Home/About.jsx
+++ main/src/component/Home/About.jsx
@@ -1,20 +1,23 @@
 import Data from "../Data.json";
+import ProfilePic from "../../assets/myPic.jpg";
 import "./Styles/About.css";
 
 const About = () => {
-    return(
-        <div className="about">
-            <div className="container">
-                <div className="text">
-                    <h2>About Me??</h2>
-                    <p>{Data.user.About}</p>
-                </div>
-                <div className="image-box">
-                    <img src={Data.user.ProfilePic} alt="myimage"/>
-                </div>
-            </div>
+  return (
+    <div className="about">
+      <div className="container">
+        <div className="text">
+          <h2>About Me??</h2>
+          <p>{Data.user.About}</p>
         </div>
-    )
-}
+        <div className="image-box">
+          <img src={ProfilePic} alt="myimage" />
+        </div>
+      </div>
+
+      <h1>Things I have Built (I am still working on them -_-)</h1>
+    </div>
+  );
+};
 
 export default About;
