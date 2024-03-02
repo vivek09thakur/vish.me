92eed49122d0b41450d0bd7483cf884d1303cbf6
Vivek Thakur <20509.vivek@gpranchi.org>
2024-03-02 20:22:34 +0530

    commit

--- main/src/component/BlogsList/BlogCards.jsx
+++ main/src/component/BlogsList/BlogCards.jsx
@@ -1,25 +1,63 @@
 import PropTypes from "prop-types";
-import { IoIosLink } from "react-icons/io";
+import { AiOutlineLike } from "react-icons/ai";
+import { PiTagSimpleBold } from "react-icons/pi";
+import "./Style/style.css";
 
-const BlogCards = ({ Blog }) => {
+const CreateBlogPosts = ({ posts }) => {
   return (
     <div className="blogCard">
-      <a href={Blog.link} target="_blank" rel="noopener noreferrer">
-        <h5 className="blogTitle">
-          <IoIosLink
-            style={{ position: "relative", top: "3px", marginRight: "2%" }}
+      <div className="user-info">
+        <img
+          src={posts.user.profile_image}
+          alt={`${posts.user.name} Profile Image`}
+          className="profile-image"
+        />
+        <p>{posts.user.name}</p>
+      </div>
+      <h2>{posts.title}</h2>
+      <div className="description">
+        <p style={{ opacity: "0.6" }}>
+          {posts.description}
+          <a
+            href={posts.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="read-more-btn"
+            style={{ marginLeft: "2%" }}
+          >
+            Read More
+          </a>
+        </p>
+      </div>
+
+      <div>
+        <p style={{ marginTop: "2%" }}>
+          <PiTagSimpleBold
+            style={{
+              position: "relative",
+              bottom: "-2px",
+              marginRight: "5px",
+            }}
           />
-          {Blog.title}
-        </h5>
-      </a>
+          {posts.tag_list.join(", ")}
+        </p>
+        <p id="reactions">
+          <AiOutlineLike
+            style={{
+              position: "relative",
+              bottom: "-1px",
+              marginRight: "5px",
+            }}
+          />
+          {posts.public_reactions_count}
+        </p>
+      </div>
     </div>
   );
+}
+
+CreateBlogPosts.propTypes = {
+  posts: PropTypes.object.isRequired,
 };
 
-BlogCards.propTypes = {
-  Blog: PropTypes.object.isRequired,
-  title: PropTypes.string,
-  link: PropTypes.string,
-};
-
-export default BlogCards;
+export default CreateBlogPosts;
--- main/src/component/BlogsList/Blogs.jsx
+++ main/src/component/BlogsList/Blogs.jsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from "react";
+import CreateBlogPosts from "./BlogCards";
 import Data from "../Data.json";
-import BlogCards from "./BlogCards";
 import "./Style/style.css";
 
 const BlogList = () => {
-  const BlogKeys = Object.keys(Data.blogs);
+  const username = Data.socials.dev;
+  const apiUrl = `https://dev.to/api/articles?username=${username}`;
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.log("Error fetching data from dev.to", error);
+        setError("Error fetching data from dev.to");
+      }
+    };
+
+    fetchBlogs();
+  }, [apiUrl]);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
     <div className="blogList">
       <h3>My Recent Blogs</h3>
-      {BlogKeys.map((key, item) => (
-        <BlogCards key={item} Blog={Data.blogs[key]} />
+      {posts.slice(0, 10).map((post) => (
+        <CreateBlogPosts key={post.id} posts={post} />
       ))}
     </div>
   );
