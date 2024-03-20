29f9d14c9e52709659f7b3601071066dc4e50b34
Vivek Thakur <20509.vivek@gpranchi.org>
2024-03-20 11:23:54 +0530

    commit

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -3,7 +3,6 @@ import SideNav from "./component/SideNav/SideNav";
 import Home from "./component/Home/Home";
 import About from "./component/Home/About";
 import BlogList from "./component/BlogsList/Blogs";
-import GithubActivity from "./component/Footer/GithubCalender/Calender";
 import Footer from "./component/Footer/Footer";
 import { Planets } from "react-preloaders";
 import { useEffect } from "react";
@@ -36,7 +35,6 @@ const App = () => {
         <Home />
         <About />
         <BlogList />
-        <GithubActivity />
         <Footer />
       </React.Fragment>
     </>
