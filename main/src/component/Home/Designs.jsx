bcc4b4ce0bfb464f8225146192623e9ffaaafb1e
Vivek Thakur <20509.vivek@gpranchi.org>
2024-09-24 13:38:42 +0530

    commit

--- main/src/component/Home/Designs.jsx
+++ main/src/component/Home/Designs.jsx
@@ -1,4 +1,5 @@
-import "./Styles/About.css";
+import { useState } from "react";
+import { X } from "lucide-react";
 
 const Designs = () => {
   const designs = [
@@ -6,24 +7,36 @@ const Designs = () => {
     "https://embed.figma.com/design/xC9oCxslWKUBKiZom2OsBG/NET_ANALYSIS_DASHBOARD?embed-host=share",
     "https://embed.figma.com/design/34h6urJBdokf9NNYMILHXZ/HIRO-UI-Design?embed-host=share",
   ];
+
+  const [interactiveIndex, setInteractiveIndex] = useState(null);
+
+  const toggleInteraction = (index) => {
+    setInteractiveIndex(interactiveIndex === index ? null : index);
+  };
+
   return (
-    <>
-      <div className="designs">
-        <div className="header">
-          <h1>UI/UX Designs</h1>
-        </div>
-        <div className="design-grid">
-          {designs.map((design, index) => (
+    <div className="designs">
+      <div className="header">
+        <h1>UI/UX Designs</h1>
+      </div>
+      <div className="design-grid">
+        {designs.map((design, index) => (
+          <div key={index} className="iframe-container relative">
             <iframe
-              key={index}
               src={design}
               allowFullScreen
-              allow="transparency"
-            ></iframe>
-          ))}
-        </div>
+              style={{
+                pointerEvents: interactiveIndex === index ? "auto" : "none",
+              }}
+              className="w-full h-full border-0"
+            />
+            <button onClick={() => toggleInteraction(index)}>
+              {interactiveIndex === index ? <X size={12} /> : "View Design"}
+            </button>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
