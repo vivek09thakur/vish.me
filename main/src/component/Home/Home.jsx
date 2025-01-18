01bc7ff820b810c84761afea1731830776e9c4e3
vivek09thakur <vivekthakurcse20509@gmail.com>
2025-01-19 00:34:11 +0530

    commit

--- main/src/component/Home/Home.jsx
+++ main/src/component/Home/Home.jsx
@@ -1,9 +1,31 @@
+import React, { useEffect, useState } from "react";
 import Data from "../Data.json";
 import { RiDownloadLine } from "react-icons/ri";
 import MyImage from "../../assets/group6.svg";
 import { CiLocationOn } from "react-icons/ci";
 import "./Styles/Style.css";
 
+const Typewriter = ({ text }) => {
+  const [displayedText, setDisplayedText] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (index < text.length) {
+        setDisplayedText((prev) => prev + text[index]);
+        setIndex((prev) => prev + 1);
+      } else {
+        setTimeout(() => {
+          setDisplayedText("");
+          setIndex(0);
+        }, 1000);
+      }
+    }, 100);
+    return () => clearTimeout(timeout);
+  }, [index, text]);
+
+  return <h1 id="myname">{displayedText}</h1>;
+};
 const Home = () => {
   return (
     <div className="home grid-overlay">
@@ -27,9 +49,9 @@ const Home = () => {
             />
             Resume
           </a>
-          <h1 id="myname">{Data.user.IntroLine}</h1>
+          <Typewriter text={Data.user.IntroLine} />
           <p>{Data.user.About}</p>
-          <h1 id="#location" style={{ opacity: "0.6", fontSize: "1.5rem" }}>
+          <h1 id="location" style={{ opacity: "0.6", fontSize: "1.5rem" }}>
             <CiLocationOn style={{ position: "relative", bottom: "-4px" }} />{" "}
             Mumbai, Maharashtra, India
           </h1>
