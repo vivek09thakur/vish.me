02a6ba1dc160f811c09feca188261861fe55a853
vivek09thakur <vivekthakurcse20509@gmail.com>
2025-12-16 22:22:01 +0530

    commit

--- main/src/component/Cards/Card.jsx
+++ main/src/component/Cards/Card.jsx
@@ -3,31 +3,39 @@
 import { RxExternalLink } from "react-icons/rx";
 import { PiTagSimple } from "react-icons/pi";
 import PropTypes from "prop-types";
+import demo_image from "/demo-project.png";
 import "./Style.css";
 
 const Cards = ({ project }) => {
   return (
     <div className="project_card">
+      <div className="project-image">
+        <img src={demo_image} alt="" sizes="" srcset="" />
+      </div>
+      <div className="project-content">
+        <h2>
+          <RxExternalLink className="project_icon" /> {project.name}
+        </h2>
+        <div className="project_description">
+          <p style={{ opacity: ".85" }}>{project.description}</p>
+          <p className="tech-stack">
+            <PiTagSimple
+              style={{
+                position: "relative",
+                bottom: "-2px",
+                paddingRight: "5px",
+              }}
+            />
+            {project.tech_stack}
+          </p>
+        </div>
+      </div>
       <a
         href={project.link}
         target="_blank"
         rel="noreferrer"
         style={{ textDecoration: "none", color: "var(--font_color1)" }}
-      >
-        <h2>
-          <RxExternalLink className="project_icon" /> {project.name}
-        </h2>
-      </a>
-
-      <div className="project_description">
-        <p style={{ opacity: ".85" }}>{project.description}</p>
-      </div>
-      <p className="tech-stack">
-        <PiTagSimple
-          style={{ position: "relative", bottom: "-2px", paddingRight: "5px" }}
-        />
-        {project.tech_stack}
-      </p>
+      ></a>
     </div>
   );
 };
